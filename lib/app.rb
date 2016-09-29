fd3a97e3d9a56531f4185b5ae854ca2dcff63d0b
Richard Bates <richo225@gmail.com>
2016-09-29 17:50:17 +0100

    Add client user count methods

--- lib/app.rb
+++ lib/app.rb
@@ -11,5 +11,17 @@ class TwitterFetcher
     config.access_token = ENV["ACCESS_TOKEN"]
     config.access_token_secret = ENV["ACCESS_SECRET"]
   end
-  
+
+  def get_followers
+    client.user.followers_count
+  end
+
+  def get_tweets
+    client.user.tweets_count
+  end
+
+  def get_mentions
+    client.user.mentions_timeline
+  end
+
 end
